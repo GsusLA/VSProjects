1d5c593010818f8a58060370a64067205c200020
GsusLA <gsus.arriaga@gmail.com>
2016-08-16 16:47:46 -0500

    Se documento la clase ManageWifi

--- Projects/Camara/Camara/ManageWifi.cs
+++ Projects/Camara/Camara/ManageWifi.cs
@@ -7,13 +7,20 @@ using NativeWifi;
 
 namespace Camara
 {
+    /** Clase que recupera los dispositivos wifi detectador disponibles y que se usa para validar si el 
+     * router predefinido( En este caso PROXIM142563 ) esta dentro del alcance del wifi de la computadora
+     en caso de que ya este en el alcance se iniciara un proceso de copiado de datos al servidor predefinido
+     que recibira los fragmentos de video
+     */
     class ManageWifi
     {
         WlanClient client;
-        public ManageWifi()
+        public ManageWifi()  //Constructor de la clase
         {
-            client = new WlanClient();
+            client = new WlanClient(); // Se inicializa la case que recupera los dispositivos Wifi
         }
+        /**Metodo que escanea los dispositivos wifi que esten en el alcance de la señal y devuelve un 
+         * true si lo ha detectado satisfactoriamente*/
         public bool scanWifi() {
 
             foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
